e4e4f8734e02e655a1b79ba1fe764c85c0c30b01
Luís Felipe Souza <66641151+lfwdev@users.noreply.github.com>
2022-06-06 16:51:08 -0300

    rmann

--- LAB II/m5/src/m5/LinkedQueue.java
+++ LAB II/m5/src/m5/LinkedQueue.java
@@ -2,55 +2,112 @@ package m5;
 // A partir da classe Node e da interface Queue, crie uma classe chamada LinkedQueue, que implementa Queue, ou seja, é uma fila, porém, encadeada. 
 public class LinkedQueue<E> implements Queue<E> {
 	
-	Node<E> front;
-	Node<E> back;
-	private int numElements;
+	Node<E> head;
+	Node<E> tail;
+	private static int numElements;
+	private static int maxNumElements = 5;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		numElements = 0;
+		
+		LinkedQueue<Integer> numberQueue = new LinkedQueue<Integer>();
+        numberQueue.enqueue(1);
+        numberQueue.enqueue(89);       
+        numberQueue.enqueue(13);
+        numberQueue.enqueue(120);
+        
+        numberQueue.dequeue();
+        numberQueue.enqueue(34);
+        numberQueue.enqueue(344);
+        numberQueue.enqueue(234);
+        numberQueue.dequeue();
+        
+ 
+        
+        System.out.println("Queue Front : " + numberQueue.front());
+        System.out.println("Queue Back : " + numberQueue.back());
+        
+        //Change Node Element Value
+        numberQueue.head.setElement(666);
+        System.out.println("Queue Front after changing node element: " + numberQueue.front());
+        
+        System.out.println("Queue Size : " + numberQueue.numElements());
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		
+        if (this.tail == null) {
+            return true;
+        }
+		
 		return false;
 	}
 
 	@Override
 	public boolean isFull() {
 		// TODO Auto-generated method stub
-		return false;
+		return numElements >= maxNumElements;
 	}
 
 	@Override
 	public int numElements() {
-		// TODO Auto-generated method stub
-		return 0;
+		return numElements;
 	}
 
+
 	@Override
-	public void enqueue(Object element) throws UnderflowException {
-		// TODO Auto-generated method stub
-		
+	public void enqueue(E element) throws UnderflowException {
+        // Create a new LinkedQueue node
+        Node<E> tempNode = new Node<E>(element);
+
+        
+        // If queue is empty, then new node is front and back
+        if (isEmpty()) {
+            this.head = this.tail = (Node<E>) tempNode;
+            numElements++;
+            return;
+        }
+ 
+        // Add the new node at the end of queue and change back
+        //this.tail.next = (Node<E>) tempNode;
+        this.tail.setNext(tempNode);
+        this.tail = (Node<E>) tempNode;
+        
+        numElements++;
 	}
 
 	@Override
 	public E dequeue() throws UnderflowException {
-		// TODO Auto-generated method stub
+		// If queue is empty, return NULL.
+        if (this.head == null)
+        	//System.out.println("Queue Front : " + q.front.key);
+            return null;
+ 
+        // Store previous front and move front one node ahead
+        Node<E> tempNode = this.head;
+        this.head = this.head.next;
+ 
+        // If front becomes NULL, then change rear also as NULL
+        if (this.head == null)
+            this.tail = null;
+        
+        numElements--;
+        
 		return null;
 	}
 
 	@Override
 	public E front() throws UnderflowException {
 		// TODO Auto-generated method stub
-		return null;
+		return head.getElement();
 	}
 
 	@Override
 	public E back() throws UnderflowException {
 		// TODO Auto-generated method stub
-		return null;
+		return tail.getElement();
 	}
 
 }
--- LAB II/m5/src/m5/LinkedStack.java
+++ LAB II/m5/src/m5/LinkedStack.java
@@ -2,48 +2,68 @@ package m5;
 // A partir da classe Node e da interface Stack, crie uma classe chamada LinkedStack, que implementa Stack, ou seja, é uma pilha, porém, encadeada. 
 public class LinkedStack<E> implements Stack<E> {
 
-	Node<E> top;
-	private int numElements;
+	Node<E> head;
+	Node<E> tail;
+	private static int numElements;
+	private static int maxNumElements = 5;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		numElements = 0;
+		
+		LinkedStack<Integer> numberStack = new LinkedStack<Integer>();
 
+		numberStack.push(5);
+		numberStack.push(4);
+		numberStack.push(3);
+		numberStack.push(2);
+		numberStack.push(1);
+		
+		
+        System.out.println("Stack Top : " + numberStack.top());
+		numberStack.pop();
+		numberStack.pop();
+        System.out.println("Stack Top : " +  numberStack.top());
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return numElements == 0;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		return numElements >= maxNumElements;
 	}
 
 	@Override
 	public int numElements() {
-		// TODO Auto-generated method stub
-		return 0;
+		return numElements;
 	}
 
 	@Override
-	public void push(Object element) throws UnderflowException {
-		// TODO Auto-generated method stub
+	public void push(E element) throws UnderflowException {
+		Node<E> oldHead = head;
+		head = (Node<E>) new Node<E>(element);
+		head.setNext(oldHead);	
+
+	    numElements++;
+	    return;
 		
 	}
 
 	@Override
 	public E pop() throws UnderflowException {
-		// TODO Auto-generated method stub
-		return null;
+	    if (top() == null) { return null; }
+	    E item = top();
+	    head = head.getNext();
+	    numElements--;
+	    return item;
 	}
 
 	@Override
 	public E top() throws UnderflowException {
 		// TODO Auto-generated method stub
-		return null;
+		return head.getElement();
 	}
 
 }
